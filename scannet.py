2d65ae9bc289304ccc423d7efcc1bcdac24f9781
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-09-28 18:07:21 +0200

    add error logging

--- scannet.py
+++ scannet.py
@@ -2,7 +2,9 @@
 
 # Scans the LAN for all hosts and reports some statistics about them
 
+import syslog, traceback
 import subprocess as sp
+import MySQLdb as mdb
 
 def storeinsql(line):
   #print line
@@ -70,96 +72,104 @@ def red(name): print ("\033[91m {}\033[00m" .format(name))
 
 if __name__ == '__main__':
   DEBUG = False
-  print "*** ScanNet ***"
-  ux = getuxtime()
-  ux = map(int,ux)[0]
-  
-  lstLease = getleases()
-  if DEBUG:print len(lstLease),"\n"
+  try:
+    print "*** ScanNet ***"
+    ux = getuxtime()
+    ux = map(int,ux)[0]
 
-  lstArp =  getarp()
-  if DEBUG:print len(lstArp),"\n"
+    lstLease = getleases()
+    if DEBUG:print len(lstLease),"\n"
 
-  # dimension the array
-  # 0 = IP
-  # 1 = hostname (dhcp lease)
-  # 2 = hostname (arp)
-  # 3 = MAC
-  # 4 = ping min
-  # 5 = ping avg
-  # 6 = ping max
-  # 7 = ping stdev
-  # 8 = IP(...4)
-  # 9 = Time to release (minutes)
-  lstOut = [[None]*10 for _ in range(len(lstLease))]
-  if DEBUG:
-    print "array size:"
-    print len(lstOut),"\n"
+    lstArp =  getarp()
+    if DEBUG:print len(lstArp),"\n"
 
-  # fill the array with data from lstLease
-  for idx, line in enumerate(lstLease):
-    if DEBUG:print idx,line
-    items = line.split()
-    # IP
-    lstOut[idx][0] = items[2]
-    # hostname
-    lstOut[idx][1] = items[3]
-    # MAC
-    lstOut[idx][3] = items[1]
-    # T2R
-    lstOut[idx][9] = (int(items[0]) - ux)/60
-  # make a list of the IPs
-  colList = [ lstOut[i][0] for i in xrange(len(lstOut)) ]
-  if DEBUG:print "\t",colList
+    # dimension the array
+    # 0 = IP
+    # 1 = hostname (dhcp lease)
+    # 2 = hostname (arp)
+    # 3 = MAC
+    # 4 = ping min
+    # 5 = ping avg
+    # 6 = ping max
+    # 7 = ping stdev
+    # 8 = IP(...4)
+    # 9 = Time to release (minutes)
+    lstOut = [[None]*10 for _ in range(len(lstLease))]
+    if DEBUG:
+      print "array size:"
+      print len(lstOut),"\n"
 
-  # Add `arp` data to the array
-  for idx,line in enumerate(lstArp):
-    if DEBUG:print idx,line
-    items = line.split()
-    ip=items[1][1:-1]
-    try:
-      adx = colList.index(ip)
-      # arp hostname
-      lstOut[adx][2] = items[0]
-    except ValueError:
-      #if DEBUG:print ip
-      lstOut = lstOut + [[None]*10]
-      adx = len(lstOut)-1
-      #colList.index(None)
-      #print lstOut
-      #print adx
-      lstOut[adx][0] = ip
-      lstOut[adx][1] = items[0]
-      lstOut[adx][3] = items[3]
-      lstOut[adx][2] = items[0]
-      lstOut[adx][9] = -1
-      colList = [ lstOut[i][0] for i in xrange(len(lstOut)) ]
-      #if DEBUG:print "\t",colList
+    # fill the array with data from lstLease
+    for idx, line in enumerate(lstLease):
+      if DEBUG:print idx,line
+      items = line.split()
+      # IP
+      lstOut[idx][0] = items[2]
+      # hostname
+      lstOut[idx][1] = items[3]
+      # MAC
+      lstOut[idx][3] = items[1]
+      # T2R
+      lstOut[idx][9] = (int(items[0]) - ux)/60
+    # make a list of the IPs
+    colList = [ lstOut[i][0] for i in xrange(len(lstOut)) ]
+    if DEBUG:print "\t",colList
 
-  lenhost=0
-  for idx,line in enumerate(lstOut):
-    ip = line[0]
-    # get 4th element of IP
-    lstOut[idx][8] = int(ip.split('.')[3])
-    # find length of longest hostname
-    if len(lstOut[idx][1]) > lenhost:
-      lenhost=len(lstOut[idx][1])
+    # Add `arp` data to the array
+    for idx,line in enumerate(lstArp):
+      if DEBUG:print idx,line
+      items = line.split()
+      ip=items[1][1:-1]
+      try:
+        adx = colList.index(ip)
+        # arp hostname
+        lstOut[adx][2] = items[0]
+      except ValueError:
+        #if DEBUG:print ip
+        lstOut = lstOut + [[None]*10]
+        adx = len(lstOut)-1
+        #colList.index(None)
+        #print lstOut
+        #print adx
+        lstOut[adx][0] = ip
+        lstOut[adx][1] = items[0]
+        lstOut[adx][3] = items[3]
+        lstOut[adx][2] = items[0]
+        lstOut[adx][9] = -1
+        colList = [ lstOut[i][0] for i in xrange(len(lstOut)) ]
+        #if DEBUG:print "\t",colList
 
-  # Output the results
-  lstOut = sorted(lstOut, key=getKey)
-  for idx,line in enumerate(lstOut):
-    # ping the host
-    ip = line[0]
-    pong =  map(float,ping(ip,1))
-    if pong[0] > 0:
-      pong =  map(float,ping(ip,10))
-    lstOut[idx][4] = pong[0]
-    lstOut[idx][5] = pong[1]
-    lstOut[idx][6] = pong[2]
-    lstOut[idx][7] = pong[3]
+    lenhost=0
+    for idx,line in enumerate(lstOut):
+      ip = line[0]
+      # get 4th element of IP
+      lstOut[idx][8] = int(ip.split('.')[3])
+      # find length of longest hostname
+      if len(lstOut[idx][1]) > lenhost:
+        lenhost=len(lstOut[idx][1])
 
-    storeinsql(line)
-    spc0 = ' ' * ( 16 - len(line[0]) )
-    spc1 = ' ' * ( lenhost - len(line[1]) + 1 )
-    spc2 = ' ' * ( 17 - len(line[3]) + 1 )
-    print line[0], spc0, line[1], spc1, line[3], spc2, "avg=", line[5], "\tstdev=", line[7], "\tT2R=", line[9]
+    # Output the results
+    lstOut = sorted(lstOut, key=getKey)
+    for idx,line in enumerate(lstOut):
+      # ping the host
+      ip = line[0]
+      pong =  map(float,ping(ip,1))
+      if pong[0] > 0:
+        pong =  map(float,ping(ip,10))
+      lstOut[idx][4] = pong[0]
+      lstOut[idx][5] = pong[1]
+      lstOut[idx][6] = pong[2]
+      lstOut[idx][7] = pong[3]
+
+      storeinsql(line)
+      spc0 = ' ' * ( 16 - len(line[0]) )
+      spc1 = ' ' * ( lenhost - len(line[1]) + 1 )
+      spc2 = ' ' * ( 17 - len(line[3]) + 1 )
+      print line[0], spc0, line[1], spc1, line[3], spc2, "avg=", line[5], "\tstdev=", line[7], "\tT2R=", line[9]
+  except Exception as e:
+    if DEBUG:
+      print("Unexpected error:")
+      print e.message
+    syslog.syslog(syslog.LOG_ALERT,e.__doc__)
+    syslog_trace(traceback.format_exc())
+    raise
