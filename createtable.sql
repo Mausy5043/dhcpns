406c4c24d5ac50237c975b83256d487ddaa7b115
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-12-20 18:04:04 +0100

    remove comments

--- createtable.sql
+++ createtable.sql
@@ -1,19 +1,4 @@
-# Below are the commands required to recreate (without data) the table for DHCPNS
-
 USE dhcpnsdb;
-# mysql> SHOW TABLES;
-# +--------------------+
-# | Tables_in_dhcpnsdb |
-# +--------------------+
-# | lantbl             |
-# +--------------------+
-# 1 row in set (0.00 sec)
-
-# +--------+
-# | Table  |
-# +--------+
-# | lantbl |
-# +--------+
 
 DROP TABLE IF EXISTS lantbl;
 
