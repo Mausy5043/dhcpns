459b251e9ace5d66fca66dc3b25c044bb78a7de9
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2017-11-11 13:25:40 +0100

    New package installation method

--- install.sh
+++ install.sh
@@ -3,8 +3,26 @@
 fversion="$HOME/.dhcpns.version"
 fbranch="$HOME/.dhcpns.branch"
 
-VERSION="1.2"
+VERSION="1.3"
 BRANCH="master"
+
+install_package()
+{
+  # See if packages are installed and install them.
+  package=$1
+  echo "*********************************************************"
+  echo "* Requesting ${package}"
+  status=$(dpkg-query -W -f='${Status} ${Version}\n' "${package}" 2>/dev/null | wc -l)
+  if [ "${status}" -eq 0 ]; then
+    echo "* Installing ${package}"
+    echo "*********************************************************"
+    sudo apt-get -yuV install "${package}"
+  else
+    echo "* Already installed !!!"
+    echo "*********************************************************"
+  fi
+}
+
 
 if [ ! -e "$fversion" ]; then
   echo $VERSION > "$fversion"
@@ -13,4 +31,7 @@ if [ ! -e "$fbranch" ]; then
   echo $BRANCH > "$fbranch"
 fi
 
-sudo apt-get -yuV install python-mysqldb mysql-client
+sudo apt-get update
+# MySQL support (python3)
+install_package "mysql-client"
+install_package "python-mysqldb"
