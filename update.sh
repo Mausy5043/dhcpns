dfaa9be8e52b5847a742d811a6331b5e327a92e1
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-12-16 11:27:11 +0100

    no need to change mode in script

--- update.sh
+++ update.sh
@@ -5,7 +5,4 @@ pushd "$HOME/dhcpns"
   git pull
   git fetch origin
   git checkout "$branch" && git reset --hard "origin/$branch" && git clean -f -d
-
-  # Set permissions
-  chmod -R 744 ./*
 popd
