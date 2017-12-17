26766c033d1962a0d964cd5b8444a3ec1baccd34
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-12-17 09:31:24 +0100

    abort if dnsmasq doesnt exist

--- getleasesfile.sh
+++ getleasesfile.sh
@@ -2,4 +2,11 @@
 
 DNSMASQCONF=/etc/dnsmasq.conf
 DNSMASQDIR=/etc/dnsmasq.d
-cat "${DNSMASQCONF}" "${DNSMASQIR}/*" | egrep "^dhcp-leasefile" | awk -F "=" '{print $2}'
+
+# abort if dnsmasq is not properly installed
+if [ ! -f "${DNSMASQCONF}" ]; then
+  echo "${DNSMASQCONF} not found. Is dnsmasq installed?"
+  exit 1
+fi
+
+cat "${DNSMASQCONF}" "${DNSMASQDIR}/*" | egrep "^dhcp-leasefile" | awk -F "=" '{print $2}'
